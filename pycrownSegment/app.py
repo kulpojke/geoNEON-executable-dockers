87d1041214a8b25f2cb76d964b2355ec1505351e
michael <michaelhuggins@protonmail.com>
2021-04-09 14:49:54 -0700

    added files in pycrownSegment

---
+++ pycrownSegment/app.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from pycrown import PyCrown
+from shapely.geometry import mapping, Point, Polygon
+import geopandas
+import numpy as np
+import fiona
+
+print('Ok fool!')
