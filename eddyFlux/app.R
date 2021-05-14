a129458bf5031013c2fa208a246169f10e4d286f
michael <michaelhuggins@protonmail.com>
2021-05-14 12:00:44 -0700

    x

--- eddyFlux/app.R
+++ eddyFlux/app.R
@@ -15,6 +15,9 @@ savepath  <- '/savepath'
 options(stringsAsFactors=F)
 
 # we'll need this
+library(parallel)
+ncores <- detectCores()
+
 library(dplyr)
 library(neonUtilities)
 
@@ -39,27 +42,49 @@ soilTID   <- 'DP1.00041.001'
 soilCO2 <- loadByProduct(soilCO2ID, site=site, 
                     timeIndex=30, package="basic", 
                     startdate=startdate, enddate=enddate,
-                    check.size=F)
+                    check.size=F, nCores=ncores)
 
 soilH2O <- loadByProduct(soilH2OID, site=site, 
                     timeIndex=30, package="basic", 
                     startdate=startdate, enddate=enddate,
-                    check.size=F)
+                    check.size=F, nCores=ncores)
 
 soilT <- loadByProduct(soilTID, site=site, 
                     timeIndex=30, package="basic", 
                     startdate=startdate, enddate=enddate,
-                    check.size=F)
+                    check.size=F, nCores=ncores)
 
 # join the data for soilT (R is so painful, this would be so much easier in python)
 positions <- unique(soilT$ST_30_minute[c("verticalPosition")])[[1]]
 
-df <- flux[[1]]
+flx <- flux[[1]]
+
+library(doParallel)
+
+# define a function for merging columns computed in ||
+merge_columns <- function(a, b) {
+    merge(a, b, by='timeBgn')
+}
+
+# define a (very boilerplate) function for getting the data within the loop
+scrape_data <- function(pos, soilT) {
+    col <- paste0('ST_', pos)
+    values <- soilT$ST_30_minute[which(soilT$ST_30_minute$verticalPosition==pos),c("startDateTime", "horizontalPosition", "soilTempMean","soilTempMinimum", "soilTempExpUncert", "soilTempStdErMean", "finalQF", "soilTempMaximum", "soilTempVariance")]
+    values$timeBgn <- values$startDateTime
+}
+
+p = length(positions)
+
+
+foreach(i=1:p, .combine=merge_columns) %dopar% {
+
+
+results = foreach()
 
 for (pos in positions) {
     col <- paste0('ST_', pos)
     values <- soilT$ST_30_minute[which(soilT$ST_30_minute$verticalPosition==pos),c("startDateTime", "horizontalPosition", "soilTempMean","soilTempMinimum", "soilTempExpUncert", "soilTempStdErMean", "finalQF", "soilTempMaximum", "soilTempVariance")]
     values$timeBgn <- values$startDateTime
-    df <- merge(values, df, by="timeBgn")
+    
 }
 
