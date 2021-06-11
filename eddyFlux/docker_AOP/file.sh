658c9991ce2e4ac3bb56969871f399606dffc492
michael <michaelhuggins@protonmail.com>
2021-06-11 13:48:28 -0700

    rearanginf edd_flux dockerfiles

---
+++ eddyFlux/docker_AOP/file.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DPID=$1
+SITE=$2
+YEAR=$3
+EASTING=$4
+NORTHING=$5
+BUFFER=$6
+TOKEN=$7
+
+
+# run the app
+Rscript AOPdownload.R  $DPID $SITE $YEAR $EASTING $NORTHING $BUFFER $TOKEN
+
+# make sure permissions are not heinous
+#chmod -R 766 /savepath
+
