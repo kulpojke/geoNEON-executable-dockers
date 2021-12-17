caf60dce65e5c865dc6c4545b35f99cad4f7614b
michael <michaelhuggins@protonmail.com>
2021-12-17 12:02:34 -0800

    changes versions and added optuna to tfp_jupyter Dockerfile

--- tfp_Jupyter/start.sh
+++ tfp_Jupyter/start.sh
@@ -3,7 +3,7 @@
 docker build docker -t tfp_jupyter_docker && \
 docker run -it --rm --runtime=nvidia -u $(id -u):$(id -g) -v /data/mthuggin/eddy2:/data -v $(realpath /data/mthuggin/notebooks):/tf/notebooks -p 8885:8888 tfp_jupyter_docker
 
-#
+# ./start.sh
 #
 # then in new terminal
 # ssh -NL localhost:1234:localhost:8885  mthuggin@elcapitan.csc.calpoly.edu
